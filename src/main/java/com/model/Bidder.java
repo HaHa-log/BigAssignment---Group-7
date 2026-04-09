88c7252b8c75430c848b7bbc241eb87d2aa4839a
Bùi Hà Linh <25020233@vnu.edu.vn>
2026-04-10 00:56:39 +0700

    Delete src/main/java/com/model directory

--- src/main/java/com/model/Bidder.java
+++
@@ -1,2 +0,0 @@
-public class Bidder extends User{
-}
--- src/main/java/com/model/Item.java
+++
@@ -1,8 +0,0 @@
-public abstract class Item extends Entity {
-    private String startPrice;
-
-    public Item(String id, String name, String startPrice) {
-        super(id, name);
-        this.startPrice = startPrice;
-    }
-}
