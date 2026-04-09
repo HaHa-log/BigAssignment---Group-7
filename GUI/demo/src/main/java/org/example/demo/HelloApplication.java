d6a6a71510595ff3c6930f9761b198730ac17c76
Bùi Hà Linh <25020233@vnu.edu.vn>
2026-04-09 22:42:32 +0700

    UI LoginPage draft

---
+++ GUI/demo/src/main/java/org/example/demo/HelloApplication.java
@@ -0,0 +1,20 @@
+package org.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240, Color.LIGHTSKYBLUE);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.show();
+    }
+}
