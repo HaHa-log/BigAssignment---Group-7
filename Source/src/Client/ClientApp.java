249d88326cbf800faa19e7d6b1174aab8d39e1c2
Bùi Hà Linh <25020233@vnu.edu.vn>
2026-04-10 14:35:19 +0700

    Update Login page

--- Source/src/Client/ClientApp.java
+++ Source/src/Client/ClientApp.java
@@ -1,5 +1,13 @@
-package Client;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import Controllers.DemoPageController;
+
+import java.io.IOException;
 
 public class ClientApp extends Application {
+    public void start(Stage demoStage) throws IOException {
+        DemoPageController demoPage = new DemoPageController();
+        demoPage.openDemoPage();
+    }
 
 }
--- Source/src/Client/Controllers/DemoPageController.java
+++ Source/src/Client/Controllers/DemoPageController.java
@@ -1,7 +1,20 @@
 package Controllers;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 
-public class DemoPageController {
-    FXMLLoader demoPage = new FXMLLoader(getClass().getResource("/DemoPage.fxml"));
+public class DemoPageController{
+    public void openDemoPage() throws IOException {
+        FXMLLoader demoPage = new FXMLLoader(getClass().getResource("/resources/DemoPage.fxml"));
+        Parent demoPageRoot = demoPage.load();
+        Scene demoPageScene = new Scene(demoPageRoot);
+
+        Stage demoPageStage = new Stage();
+        demoPageStage.setFullScreen(true);
+        demoPageStage.setScene(demoPageScene);
+        demoPageStage.show();
+    }
 }
