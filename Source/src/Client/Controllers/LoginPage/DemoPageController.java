5b52ce6d4bf435f23b143b90d93f52188ceefbef
Bùi Hà Linh <25020233@vnu.edu.vn>
2026-04-11 23:26:17 +0700

    Update

--- Source/src/Client/Controllers/LoginPage/DemoPageController.java
+++ Source/src/Client/Controllers/LoginPage/DemoPageController.java
@@ -1,12 +1,17 @@
 package Client.Controllers.LoginPage;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class DemoPageController{
+    @FXML
+    private StackPane ContentPane;
+
     public void openDemoPage() throws IOException {
         FXMLLoader demoPage = new FXMLLoader(getClass().getResource("/Client/resources/DemoPage.fxml"));
         Parent demoPageRoot = demoPage.load();
@@ -16,5 +21,25 @@ public class DemoPageController{
         demoPageStage.setFullScreen(true);
         demoPageStage.setScene(demoPageScene);
         demoPageStage.show();
+    }
+
+    public void showRegister() throws IOException {
+        FXMLLoader registerLoad = new FXMLLoader(getClass().getResource("/Client/resources/Register.fxml"));
+        Parent registerContent = registerLoad.load();
+
+        RegisterController controller = registerLoad.getController();
+        controller.setMainController(this);
+
+        ContentPane.getChildren().setAll(registerContent);
+    }
+
+    public void showLogin() throws IOException {
+        FXMLLoader loginLoad = new FXMLLoader(getClass().getResource("/Client/resources/Login.fxml"));
+        Parent loginContent = loginLoad.load();
+
+        RegisterController controller = loginLoad.getController();
+        controller.setMainController(this);
+
+        ContentPane.getChildren().setAll(loginContent);
     }
 }
