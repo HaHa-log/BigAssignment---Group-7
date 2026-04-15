49fbf748c1aa92305d04cbdea2d439eeb318e7b8
Ánh Ngọc <Sophiescarletthoang@gmail.com>
2026-04-15 19:58:24 +0700

    Updated placeBid

--- src/main/java/Branch/Member.java
+++ src/main/java/Branch/Member.java
@@ -1,13 +1,8 @@
 package Branch;
 
 public class Member extends User implements Bidder, Seller {
-    public Member(int id, String firstName, String lastName, String email, String phoneNumber, String password) {
-        super(id, firstName, lastName, email, phoneNumber,password);
-    }
-
-    @Override
-    public void placeBid(int auctionId, double amount) {
-        System.out.println("Placing bid...");
+    public Member(int id, String firstName, String lastName, String email, String phoneNumber, String password, double balance) {
+        super(id, firstName, lastName, email, phoneNumber,password, balance);
     }
 
     @Override
