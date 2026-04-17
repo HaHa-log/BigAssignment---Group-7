077de7d24bf662e7017cf3e974659744175d43e4
Bùi Hà Linh <25020233@vnu.edu.vn>
2026-04-17 21:14:36 +0700

    Update HomePage
    +Home Page now have shortcuts to both Profile and Auction List.
    +Add mini view (shortcut to) Auction List and Profile

--- src/main/java/Client/Controllers/MainPage/HomePageController.java
+++ src/main/java/Client/Controllers/MainPage/HomePageController.java
@@ -2,32 +2,13 @@ package Client.Controllers.MainPage;
 
 import Client.Controllers.SceneManager;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.TilePane;
-import javafx.scene.layout.VBox;
 
-import java.io.IOException;
 
 public class HomePageController{
-    @FXML
-    private TilePane auctionTilePane;
 
     @FXML
-    private void initialize() {
-        loadLimitedAuctions();
-    }
-
-    private void loadLimitedAuctions() {
-        for (int i = 0; i < 4; i ++) {
-            try {
-                FXMLLoader cardLoad = new FXMLLoader(getClass().getResource("/AuctionPageFXML/AuctionCard.fxml"));
-                VBox auctionCard = cardLoad.load();
-
-                auctionTilePane.getChildren().add(auctionCard);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+    private void toProfilePage() {
+        SceneManager.switchContent("/MainPageFXML/HomePage.fxml");
     }
 
     @FXML
