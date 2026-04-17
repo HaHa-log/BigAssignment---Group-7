98301a7b1a03fbecd6fecf3d19f7f3d4fec89d0e
Bùi Hà Linh <25020233@vnu.edu.vn>
2026-04-17 14:41:41 +0700

    UI update

--- src/main/java/Client/ClientApp.java
+++ src/main/java/Client/ClientApp.java
@@ -9,7 +9,6 @@ import javafx.stage.Stage;
 public class ClientApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        AuthService.registerNewUser("Admin", "123", "admin@gmail.com", "0123456789", "000000");
         SceneManager.setStage(stage);
         SceneManager.switchScene("/LoginFXML/DemoPage.fxml");
     }
