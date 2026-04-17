474386a24ac66ac6974a2776914775a3f1453070
Bùi Hà Linh <25020233@vnu.edu.vn>
2026-04-17 21:46:43 +0700

    Update shortcut in AuctionList to AuctionDetail

--- src/main/java/Client/Controllers/AuctionPage/AuctionDetailController.java
+++ src/main/java/Client/Controllers/AuctionPage/AuctionDetailController.java
@@ -1,4 +1,11 @@
 package Client.Controllers.AuctionPage;
 
+import Client.Controllers.SceneManager;
+import javafx.fxml.FXML;
+
 public class AuctionDetailController {
+    @FXML
+    private void toAuctionDetail() {
+        SceneManager.switchContent("/AuctionPageFXML/AuctionDetail.fxml");
+    }
 }
