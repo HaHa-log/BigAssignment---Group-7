d4e0385787bf55fef28822506d1906b5ee28265f
Bùi Hà Linh <25020233@vnu.edu.vn>
2026-04-22 13:06:15 +0700

    Add media-upload field for Create Auction
    + Media upload for Auction Create has been set up
    + Need to add a database to save the images

--- src/main/java/Client/Controllers/AuctionPage/AuctionCreateController.java
+++ src/main/java/Client/Controllers/AuctionPage/AuctionCreateController.java
@@ -4,12 +4,22 @@ import Branch.AuctionManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class AuctionCreateController {
     @FXML
     private TextField itemNameInput;
     @FXML
     private TextField descriptionInput;
+    @FXML
+    private Label fileNameLabel;
+    @FXML
+    private ImageView imagePreview;
 
     public AuctionManager auction = AuctionManager.getInstance();
 
@@ -17,5 +27,22 @@ public class AuctionCreateController {
     private void createAuction() {
         String itemName = itemNameInput.getText();
         String description = descriptionInput.getText();
+    }
+
+    //Will display image but only on the owner's device
+    //Have to save the image into database to display for other users
+    @FXML
+    private void handleChooseImage() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg")
+        );
+        File selectedImageFile = fileChooser.showOpenDialog(new Stage());
+
+        if (selectedImageFile != null) {
+            fileNameLabel.setText(selectedImageFile.getName());
+            Image image = new Image(selectedImageFile.toURI().toString());
+            imagePreview.setImage(image);
+        }
     }
 }
