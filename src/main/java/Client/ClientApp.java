1fd7527b139383c9e38329532c886c31b200d691
Bùi Hà Linh <25020233@vnu.edu.vn>
2026-05-01 22:33:31 +0700

    Update ClientApp.java

--- src/main/java/Client/ClientApp.java
+++ src/main/java/Client/ClientApp.java
@@ -11,20 +11,22 @@ import model.UsersDAO;
 import model.impl.DaoFactory;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ClientApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         UsersDAO userDb = DaoFactory.createUsersDAO();
         ItemsDAO itemDb = DaoFactory.createItemDAO();
-        Item testItem = new Item("Three body problem", 30, "A sci fi book");
-
+        List<Item> items = itemDb.getAll();
         //run the 1st line or register any user with the email "admin@gmail.com" if there is no admin@gmail.com on ur device
         //Member ownerTest = AuthService.registerNewUser("admin", "123", "admin@gmail.com", "0988172919", "000000");
         //run the 2nd line if there is already admin@gmail.com on ur device
         Member ownerTest = (Member) TempDatabase.getUserByEmail("admin@gmail.com");
 
-        AuctionManager.getInstance().createAuction(ownerTest, testItem, LocalDateTime.now(), null);
+        for (Item item : items) {
+            AuctionManager.getInstance().createAuction(ownerTest, item, LocalDateTime.now(), null);
+        }
 
         SceneManager.setStage(stage);
         SceneManager.startApp();
