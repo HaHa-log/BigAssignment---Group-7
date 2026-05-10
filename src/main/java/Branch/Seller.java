bfccde49c7100808631fbbe403f933e1b8fb6e03
Ánh Ngọc <Sophiescarletthoang@gmail.com>
2026-05-10 22:27:39 +0700

    New updates
    feat: enhance auction creation logic and fix data integrity
    
    feat: implement custom exception handling (Authentication, InvalidBid) for UI feedback.
    
    fix: prevent orphaned items in DB by validating seller status before saving.
    
    refactor: streamline Seller interface with default validation logic.
    
    fix: handle empty input and date parsing to prevent application crash.

--- src/main/java/Branch/Seller.java
+++ src/main/java/Branch/Seller.java
@@ -1,9 +1,31 @@
 package Branch;
 
+import Branch.Exceptions.*;
+import java.time.LocalDateTime;
+
 public interface Seller {
-    default boolean createAuction(Item item, double startingPrice) {
-        System.out.println("Selling stuff...");
-        return true;
-        //Tạm thời để như này
-    }
+    default void createAuction(Item item, LocalDateTime createdAt, LocalDateTime terminatedAt) {
+        if (this instanceof Member owner) {
+            if (owner.isBlocked()) {
+                throw new AuthenticationException("Your account is currently blocked and cannot create auctions.");
+            }
+
+            if (terminatedAt == null || createdAt == null) {
+                throw new CustomisedException("Scheduled auctions must have both starting and ending time");
+            }
+
+            if (terminatedAt.isBefore(LocalDateTime.now())) {
+                throw new CustomisedException("The auction termination time must be in the future.");
+            }
+
+            if (terminatedAt.isBefore(createdAt)) {
+                throw new CustomisedException("Ending time cannot be before starting time.");
+            }
+
+            try {
+                AuctionManager.getInstance().createAuction(owner, item, createdAt, terminatedAt);
+                System.out.println("[System]: Auction created successfully for item: " + item.getName());
+            } catch (Exception e) {
+                throw new CustomisedException("System error: " + e.getMessage());            }
+        }   }
 }
