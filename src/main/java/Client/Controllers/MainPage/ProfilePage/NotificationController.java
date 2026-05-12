9ec1a5654118dfd2ee469824ff73facec3865080
MaiUyen <25020424@vnu.edu.vn>
2026-05-12 10:48:40 +0700

    Load notifications asynchronously
    Fetch notifications off the JavaFX UI thread using a Task to avoid freezing the UI.

--- src/main/java/Client/Controllers/MainPage/ProfilePage/NotificationController.java
+++ src/main/java/Client/Controllers/MainPage/ProfilePage/NotificationController.java
@@ -7,6 +7,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.concurrent.Task;
+import javafx.scene.control.ProgressIndicator;
 
 public class NotificationController extends BaseController {
 
@@ -20,6 +22,9 @@ public class NotificationController extends BaseController {
     private Button clearBtn;
 
     private final ObservableList<String> allNotifications = FXCollections.observableArrayList();
+
+    @FXML
+    private ProgressIndicator loadingIndicator;
 
     @FXML
     public void initialize() {
@@ -53,12 +58,38 @@ public class NotificationController extends BaseController {
     }
 
     private void loadNotifications() {
-        System.out.println(user);
 
-        allNotifications.clear();
-        allNotifications.addAll(user.getNotifications(AuctionManager.getInstance().getAllSessions()));
-        notificationList.setItems(allNotifications);
+        if (user == null) return;
 
+        loadingIndicator.setVisible(true);
+
+        Task<ObservableList<String>> task = new Task<>() {
+
+            @Override
+            protected ObservableList<String> call() {
+
+                return FXCollections.observableArrayList(
+                        user.getNotifications(
+                                AuctionManager.getInstance().getAllSessions()
+                        )
+                );
+            }
+        };
+
+        task.setOnSucceeded(event -> {
+
+            allNotifications.setAll(task.getValue());
+
+            notificationList.setItems(allNotifications);
+
+            loadingIndicator.setVisible(false);
+        });
+
+        task.setOnFailed(event -> {
+            loadingIndicator.setVisible(false);
+        });
+
+        new Thread(task).start();
     }
 
     private void applyFilter() {
