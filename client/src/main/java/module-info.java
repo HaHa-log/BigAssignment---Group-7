7292285acbf7489b94d87014827d15f0b9e35dd3
Bùi Hà Linh <25020233@vnu.edu.vn>
2026-05-22 00:27:35 +0700

    Update module-info client side

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -1,29 +1,25 @@
 module client {
-        requires common;
+    requires common;
 
-        requires java.sql;
-        requires java.desktop;
-        requires java.prefs;
-        requires javafx.fxml;
-        requires javafx.graphics;
-        requires javafx.controls;
-        requires java.net.http;
-        requires com.fasterxml.jackson.databind;
-        requires com.fasterxml.jackson.datatype.jsr310;
+    requires java.sql;
+    requires java.desktop;
+    requires java.prefs;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires java.net.http;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
 
-        exports app to javafx.graphics;
+    exports app to javafx.graphics;
 
-        opens controllers to javafx.fxml;
-        opens controllers.LoginPage to javafx.fxml;
-        opens controllers.AuctionPage to javafx.fxml;
-        opens controllers.MainPage.ProfilePage to javafx.fxml;
-        opens controllers.MainPage.HomePage to javafx.fxml;
-        opens controllers.Management to javafx.fxml;
-        opens controllers.Inventory to javafx.fxml;
+    opens controllers to javafx.fxml;
+    opens controllers.LoginPage to javafx.fxml;
+    opens controllers.AuctionPage to javafx.fxml;
+    opens controllers.MainPage.ProfilePage to javafx.fxml;
+    opens controllers.MainPage.HomePage to javafx.fxml;
+    opens controllers.Management to javafx.fxml;
+    opens controllers.Inventory to javafx.fxml;
 
-        opens models to javafx.fxml;
-
-        opens services.dto.auth to com.fasterxml.jackson.databind;
-        opens services.dto.auction to com.fasterxml.jackson.databind;
-        opens services.dto.user to com.fasterxml.jackson.databind;
+    opens models to javafx.fxml;
 }
